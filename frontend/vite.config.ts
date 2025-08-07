2b78bbee7302d6a4da57837e77d64703ec61041c
EgorBykovsky <egor68534@gmail.com>
2025-08-07 12:39:42 +0500

    fix bug

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,20 +3,13 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
-import { visualizer } from 'rollup-plugin-visualizer';
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
     vue(),
-    VueDevTools(),
-    visualizer({
-    filename: 'dist/report.html',
-    open: true, 
-    gzipSize: true,
-    brotliSize: true,
-  }),
+    VueDevTools(),    
   ],
   resolve: {
     alias: {
