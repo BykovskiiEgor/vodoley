89b7af9dbb0641cf7932107a01700b242d1a8abf
EgorBykovsky <egor68534@gmail.com>
2025-08-08 11:57:17 +0500

    fix frontend problems

--- frontend/src/components/MobileBanners.vue
+++ frontend/src/components/MobileBanners.vue
@@ -2,42 +2,42 @@
     <div class="mobile-gallery" >
         <div class="mobile-gallery-item">
             <router-link :to="`/products/${235}`">
-                <img src="../assets/mix.jpg" alt="Mix">
+                <img src="/images/mix.jpg" alt="Mix">
             </router-link>
         </div>
         <div class="mobile-gallery-item">
             <router-link :to="`/products/${238}`">
-                <img src="../assets/shower.jpg" alt="Shower">
+                <img src="/images/shower.jpg" alt="Shower">
             </router-link>
         </div>
         <div class="mobile-gallery-item">
             <router-link :to="`/products/${168}`" >
-                <img src="../assets/wash.jpg" alt="Wash">
+                <img src="/images/wash.jpg" alt="Wash">
             </router-link>            
         </div>
         <div class="mobile-gallery-item">
             <router-link :to="`/products/${243}`">
-                <img src="../assets/toilet.jpg" alt="Toilet">
+                <img src="/images/toilet.jpg" alt="Toilet">
             </router-link>
         </div>
         <div class="mobile-gallery-item">
             <router-link :to="`/products/${247}`">
-                <img src="../assets/install.jpg" alt="Install">
+                <img src="/images/install.jpg" alt="Install">
             </router-link>            
         </div>
         <div class="mobile-gallery-item">
             <router-link :to="`/products/${44}`">
-                <img src="../assets/pp.jpg" alt="PP">
+                <img src="/images/pp.jpg" alt="PP">
             </router-link>            
         </div>
         <div class="mobile-gallery-item">
             <router-link :to="`/products/${88}`">
-                <img src="../assets/plumb.jpg" alt="Plumb">
+                <img src="/images/plumb.jpg" alt="Plumb">
             </router-link>
         </div>
         <div class="mobile-gallery-item">
             <router-link :to="`/products/${243}`">
-                <img src="../assets/toilet.jpg" alt="Toilet">
+                <img src="images/toilet.jpg" alt="Toilet">
             </router-link>
         </div>
     </div>
