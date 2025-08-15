dfe1b1d3b6814b72641d1ecbfa8da5e35210e496
EgorBykovsky <egor68534@gmail.com>
2025-08-15 22:39:16 +0500

    previous version

--- backend/items/management/commands/update_flexi.py
+++ backend/items/management/commands/update_flexi.py
@@ -1,134 +1,40 @@
-from django.core.management.base import BaseCommand
-from items.models import Item, ItemImage, ItemAttribute, Attribute
+import os
+import asyncio
 from dataclasses import dataclass
 from collections.abc import Generator
 from defusedxml.ElementTree import fromstring
+from django.core.management.base import BaseCommand
 from django.core.files.base import ContentFile
-import requests
-import os
-import httpx
-import asyncio
 from asgiref.sync import sync_to_async
+from aiohttp import ClientSession
+import httpx
 
+from items.models import Item, ItemImage, ItemAttribute, Attribute
 
 flexi_url = os.getenv("FLEXI_URL")
 
 
 @dataclass
 class ItemParse:
-    """Класс для хранения данных о товаре."""
-
     article: str
     pictures: list
     description: str | None
-    features: str | None
+    features: dict | None
 
 
 class Command(BaseCommand):
-    """Команда для обновления товаров из Flexi."""
-
     help = "Update all items from flexi"
 
-    async def download_image(self, url: str, product):
-        """Загрузка изображения."""
-        if not url.startswith(("http", "https")):
-            self.stdout.write(self.style.ERROR("URL must start with 'http:' or 'https:'"))
-            return
-        try:
-            async with httpx.AsyncClient(http2=True) as client:
-                response = await client.get(url)
-                if response.status_code == 200:
-                    img_data = response.content
-                    item_image = ItemImage(item=product)
-                    await sync_to_async(item_image.image.save, thread_sensitive=True)(
-                        os.path.basename(url),
-                        ContentFile(img_data),
-                        save=True,
-                    )
-                    self.stdout.write(self.style.SUCCESS(f"Added image from {url}."))
-                else:
-                    self.stdout.write(self.style.ERROR(f"Error downloading image from {url}. HTTP Status: {response.status_code}"))
-        except httpx.RequestError as ex:
-            self.stdout.write(self.style.ERROR(f"Error downloading image from {url}: {ex}"))
-
-    def process_features(self, features: str):
+    def process_features(self, features: str) -> dict:
         parsed_features = {}
         if features:
             for feature in features.split(";"):
                 if ":" in feature:
                     key, value = feature.split(":", 1)
                     parsed_features[key.strip()] = value.strip()
-
         return parsed_features
 
-    async def save_attributes(self, product, **attributes):
-        """Сохраняет атрибуты товара, создавая или обновляя только при изменении."""
-        try:
-            self.stdout.write(self.style.NOTICE(f"Обработка атрибутов для товара: {product.article}"))
-
-            for attr_name, attr_data in attributes.items():
-                if not attr_data:
-                    continue
-
-                attr_label, attr_value = attr_data
-                if attr_value is None:
-                    continue
-
-                self.stdout.write(self.style.NOTICE(f"Атрибут: {attr_label} → {attr_value}"))
-
-                attribute_obj, created = await sync_to_async(Attribute.objects.get_or_create, thread_sensitive=True)(
-                    name=attr_label,
-                )
-
-                if created:
-                    self.stdout.write(self.style.SUCCESS(f"Создан новый атрибут: {attr_label}"))
-
-                try:
-                    item_attr = await sync_to_async(ItemAttribute.objects.get, thread_sensitive=True)(
-                        item=product,
-                        attribute=attribute_obj,
-                    )
-                    if item_attr.value != attr_value:
-                        item_attr.value = attr_value
-                        await sync_to_async(item_attr.save, thread_sensitive=True)()
-                        self.stdout.write(self.style.SUCCESS(f"Атрибут {attr_label} обновлён для {product.article}"))
-                    else:
-                        self.stdout.write(self.style.NOTICE(f"Атрибут {attr_label} не изменился для {product.article}"))
-                except ItemAttribute.DoesNotExist:
-                    await sync_to_async(ItemAttribute.objects.create, thread_sensitive=True)(
-                        item=product,
-                        attribute=attribute_obj,
-                        value=attr_value,
-                    )
-                    self.stdout.write(self.style.SUCCESS(f"Атрибут {attr_label} добавлен для {product.article}"))
-
-        except Exception as ex:
-            self.stdout.write(self.style.ERROR(f"Ошибка при сохранении атрибутов: {str(ex)}"))
-            raise
-
-    async def add_description(self, article, description):
-        try:
-            self.stdout.write(self.style.WARNING(f"Артикул - {article} Описание - {description}"))
-            if description is None:
-                return
-
-            item = await sync_to_async(Item.objects.get, thread_sensitive=True)(article=article)
-
-            if item.description != description:
-                item.description = description
-                await sync_to_async(item.save, thread_sensitive=True)()
-                self.stdout.write(self.style.SUCCESS(f"Описание для {article} обновлено"))
-            else:
-                self.stdout.write(self.style.NOTICE(f"Описание для {article} не изменилось"))
-
-        except Item.DoesNotExist:
-            self.stdout.write(self.style.ERROR(f"Товар с артикулом {article} не найден"))
-        except Exception as ex:
-            self.stdout.write(self.style.ERROR(f"Ошибка при сохранении описания: {str(ex)}"))
-            raise
-
     def process_file(self, text: str) -> Generator[ItemParse, None, None]:
-        """."""
         try:
             root = fromstring(text)
             offers = root.findall(".//offer")
@@ -146,27 +52,95 @@ class Command(BaseCommand):
                         description,
                         parsed_features,
                     )
-
         except Exception as e:
             self.stdout.write(self.style.ERROR(f"Error parsing XML: {e}"))
 
-    def download_data(self) -> str | None:
-        """Загрузка данных с Flexi."""
+    async def download_data(self) -> str | None:
+        """Загрузка XML с Flexi."""
         if not flexi_url:
             self.stdout.write(self.style.ERROR("FLEXI_URL is not defined."))
             return None
+
         try:
-            response = requests.get(flexi_url, timeout=30)
-            if response.status_code == 200:
-                return response.text
-            else:
-                self.stdout.write(self.style.ERROR(f"Failed to fetch data. HTTP Status: {response.status_code}"))
+            async with ClientSession() as session:
+                async with session.get(flexi_url, timeout=30) as response:
+                    if response.status == 200:
+                        return await response.text()
+                    else:
+                        self.stdout.write(self.style.ERROR(f"Failed to fetch data. HTTP Status: {response.status}"))
         except Exception as ex:
             self.stdout.write(self.style.ERROR(f"Error downloading data: {ex}"))
         return None
 
+    async def download_image(self, url: str, product):
+        """Асинхронная загрузка изображения."""
+        if not url.startswith(("http", "https")):
+            self.stdout.write(self.style.ERROR("URL must start with 'http:' or 'https:'"))
+            return
+
+        try:
+            async with httpx.AsyncClient(http2=True) as client:
+                response = await client.get(url)
+                if response.status_code == 200:
+                    img_data = response.content
+                    item_image = ItemImage(item=product)
+                    await sync_to_async(item_image.image.save, thread_sensitive=True)(
+                        os.path.basename(url),
+                        ContentFile(img_data),
+                        save=True,
+                    )
+                    self.stdout.write(self.style.SUCCESS(f"Added image from {url}."))
+                else:
+                    self.stdout.write(self.style.ERROR(f"Error downloading image from {url}. HTTP Status: {response.status_code}"))
+        except httpx.RequestError as ex:
+            self.stdout.write(self.style.ERROR(f"Error downloading image from {url}: {ex}"))
+
+    async def save_attributes(self, product, **attributes):
+        """Сохранение атрибутов товара."""
+        self.stdout.write(self.style.NOTICE(f"Обработка атрибутов: {product.article}"))
+
+        for attr_name, attr_data in attributes.items():
+            if not attr_data:
+                continue
+            attr_label, attr_value = attr_data
+            if attr_value is None:
+                continue
+
+            attribute_obj, created = await sync_to_async(Attribute.objects.get_or_create, thread_sensitive=True)(name=attr_label)
+
+            if created:
+                self.stdout.write(self.style.SUCCESS(f"Создан новый атрибут: {attr_label}"))
+
+            try:
+                item_attr = await sync_to_async(ItemAttribute.objects.get, thread_sensitive=True)(item=product, attribute=attribute_obj)
+
+                if item_attr.value != attr_value:
+                    item_attr.value = attr_value
+                    await sync_to_async(item_attr.save, thread_sensitive=True)()
+                    self.stdout.write(self.style.SUCCESS(f"Атрибут {attr_label} обновлён для {product.article}"))
+            except ItemAttribute.DoesNotExist:
+                await sync_to_async(ItemAttribute.objects.create, thread_sensitive=True)(
+                    item=product,
+                    attribute=attribute_obj,
+                    value=attr_value,
+                )
+                self.stdout.write(self.style.SUCCESS(f"Атрибут {attr_label} добавлен для {product.article}"))
+
+    async def add_description(self, article, description):
+        """Обновление описания товара."""
+        if description is None:
+            return
+        try:
+            item = await sync_to_async(Item.objects.get, thread_sensitive=True)(article=article)
+            if item.description != description:
+                item.description = description
+                await sync_to_async(item.save, thread_sensitive=True)()
+                self.stdout.write(self.style.SUCCESS(f"Описание для {article} обновлено"))
+        except Item.DoesNotExist:
+            self.stdout.write(self.style.ERROR(f"Товар {article} не найден"))
+
     async def handle_async(self, data: str, items_articles: list):
-        """Асинхронная обработка данных."""
+        """Обработка данных асинхронно."""
         parsed_items = {item.article: item for item in self.process_file(data)}
         tasks = []
         semaphore = asyncio.Semaphore(10)
@@ -175,66 +149,44 @@ class Command(BaseCommand):
             async with semaphore:
                 await self.download_image(url, product)
 
-        async with httpx.AsyncClient(http2=True):
-            for article in items_articles:
-                item = None
-                if article.article in parsed_items:
-                    item = parsed_items[article.article]
-                else:
-                    article_parts = set(article.article.split("/"))
+        for product in items_articles:
+            item = parsed_items.get(product.article)
+            if not item:
+                article_parts = set(product.article.split("/"))
+                matched_key = next(
+                    (key for key in parsed_items if set(key.split("/")) & article_parts),
+                    None,
+                )
+                if matched_key:
+                    item = parsed_items[matched_key]
 
-                    matched_key = next(
-                        (key for key in parsed_items if set(key.split("/")) & article_parts),
-                        None,
-                    )
+            if item:
+                await self.add_description(product.article, item.description)
+                await self.save_attributes(
+                    product=product,
+                    **{k: (k, v) for k, v in (item.features or {}).items()},
+                )
 
-                    if matched_key:
-                        item = parsed_items[matched_key]
+                has_images = await sync_to_async(product.images.exists, thread_sensitive=True)()
+                if has_images:
+                    self.stdout.write(self.style.WARNING(f"Product {product.article} already has images, skipping download."))
+                    continue
 
-                if item:
-                    await self.add_description(
-                        article.article,
-                        item.description,
-                    )
-                    await self.save_attributes(
-                        product=article,
-                        **{key: (key, value) for key, value in (item.features or {}).items()},
-                    )
+                for url in item.pictures:
+                    tasks.append(asyncio.create_task(limited_download(url, product)))
 
-                    has_images = await sync_to_async(article.images.exists, thread_sensitive=True)()
-                    if has_images:
-                        self.stdout.write(self.style.WARNING(f"Product {article.article} already has images, skipping download."))
-                        continue
+        results = await asyncio.gather(*tasks, return_exceptions=True)
+        for idx, result in enumerate(results):
+            if isinstance(result, Exception):
+                self.stderr.write(f"Task {idx} failed with error: {result}")
 
-                    for url in item.pictures:
-                        tasks.append(
-                            asyncio.create_task(
-                                limited_download(
-                                    url,
-                                    article,
-                                )
-                            )
-                        )
+    def handle(self, *args, **options):
+        asyncio.run(self.main_async())
 
-            results = await asyncio.gather(*tasks, return_exceptions=True)
-            for idx, result in enumerate(results):
-                if isinstance(result, Exception):
-                    self.stderr.write(f"Task {idx} failed with error: {result}")
-
-    def handle(self, *args: tuple, **options: dict):
-        """Основной метод для выполнения команды."""
-        data = self.download_data()
+    async def main_async(self):
+        data = await self.download_data()
         if not data:
             return
-
-        # items_articles = list(Item.objects.annotate(
-        #     has_image=Exists(
-        #         ItemImage.objects.filter(item_id=OuterRef('id')),
-        #     ),
-        # ).filter(has_image=False))
-
-        items_articles = list(Item.objects.all())
-
+        items_articles = await sync_to_async(list)(Item.objects.all())
         self.stdout.write(self.style.SUCCESS(f"Processing {len(items_articles)} items"))
-
-        asyncio.run(self.handle_async(data, items_articles))
+        await self.handle_async(data, items_articles)
--- backend/items/management/commands/update_products.py
+++ backend/items/management/commands/update_products.py
@@ -3,34 +3,27 @@ from django.core.management.base import BaseCommand
 from items.models import Item
 
 
-def normalize_article(article):
-    if pd.isna(article):
-        return None
-    article = str(article).strip()
-    if article.endswith(".0"):
-        article = article[:-2]
-    return article.replace("\xa0", " ").strip().lower()
-
-
 class Command(BaseCommand):
     help = "Update products from Excel file"
 
     def add_arguments(self, parser):
         parser.add_argument("file_path", type=str, help="Путь к Excel файлу")
 
-    def check_actuality(self, list_to_delete, list_actual):
-        delete_set = set(filter(None, map(normalize_article, list_to_delete)))
-        actual_set = set(filter(None, map(normalize_article, list_actual)))
-        to_remove = delete_set - actual_set
+    def check_actuality(self, list_to_delite, list_actual):
+        actual_data = set(list_to_delite) - set(list_actual)
 
-        if to_remove:
-            db_items = Item.objects.all()
-            items_to_remove = [item for item in db_items if normalize_article(item.article) in to_remove]
-            for item in items_to_remove:
+        for article in actual_data:
+            try:
+                item = Item.objects.get(article=article)
                 print(f"Ненужный товар {item.name}:{item.article}")
-            Item.objects.filter(id__in=[item.id for item in items_to_remove]).delete()
+                item.delete()
+            except Item.DoesNotExist:
+                pass
 
     def handle(self, **kwargs):
+        list_to_delite = []
+        list_actual = []
+
         file_path = kwargs["file_path"]
         df = pd.read_excel(file_path)
 
@@ -40,115 +33,79 @@ class Command(BaseCommand):
         ignore_until = "Ванна поддоны"
         parsing_started = False
 
-        rows_to_process = []
-        list_to_delete = []
-        list_actual = []
-        normalized_articles = set()
+        def filtered_rows(data):
+            nonlocal parsing_started
+            for row in data:
+                if all(pd.isna(cell) for cell in row):
+                    continue
 
-        for row in data:
-            if all(pd.isna(cell) for cell in row):
-                continue
+                row_str = str(row[0])
 
-            name_cell = str(row[0])
-            raw_article = normalize_article(row[2])
+                if not parsing_started:
+                    article = str(row[2]).strip().lower()
+                    list_to_delite.append(article)
+                    if row_str.strip() == ignore_until:
+                        parsing_started = True
+                    continue
 
-            if not parsing_started:
-                if raw_article:
-                    list_to_delete.append(raw_article)
-                if name_cell.strip() == ignore_until:
-                    parsing_started = True
-                continue
+                if all(pd.isna(row[i]) for i in range(1, 4)):
+                    continue
 
-            if all(pd.isna(row[i]) for i in range(1, 4)):
-                continue
+                yield row
 
-            if raw_article:
-                normalized_articles.add(raw_article)
-            rows_to_process.append(row)
+        for row in filtered_rows(data):
 
-        db_items = Item.objects.all()
-        existing_map = {normalize_article(item.article): item for item in db_items if normalize_article(item.article) in normalized_articles}
-
-        print(f"Найдено совпадений в базе: {len(existing_map)} из {len(normalized_articles)}")
-
-        items_to_create = []
-        items_to_update = []
-
-        for row in rows_to_process:
             name = row[0]
             quantity = row[1]
+            article = str(row[2]).strip().lower()
             price = row[3]
-            article_raw = row[2]
-            article = normalize_article(article_raw)
 
-            if not article:
-                print(f"Пропущен товар без артикула: {name}")
-                continue
+            try:
+                list_actual.append(article)
+                product = Item.objects.get(article__iexact=article)
+                if pd.isna(quantity):
+                    quantity = 0
 
-            list_actual.append(article)
+                if product.name != name or product.price != price or product.quantity != quantity:
+                    product.name = name
+                    product.price = price
+                    product.quantity = quantity
+                    product.save(update_fields=["name", "price", "quantity"])
 
-            if pd.isna(quantity):
-                quantity = 0
+                if pd.isna(quantity) or quantity == 0:
+                    available = False
+                    quantity_status = "Привезем под заказ"
+                else:
+                    available = True
+                    quantity_status = "В наличии"
+                    if quantity < 2:
+                        quantity_status = "Скоро закончится"
 
-            available = quantity > 0
-            if not available:
-                quantity_status = "Привезем под заказ"
-            elif quantity < 2:
-                quantity_status = "Скоро закончится"
-            else:
-                quantity_status = "В наличии"
+                if product.available != available or product.quantity_status != quantity_status:
+                    product.available = available
+                    product.quantity_status = quantity_status
+                    product.save(update_fields=["available", "quantity_status"])
 
-            existing_item = existing_map.get(article)
+            except Item.DoesNotExist:
+                if pd.isna(quantity):
+                    available = False
+                    quantity_status = "Привезем под заказ"
+                    quantity = 0
+                else:
+                    available = True
+                    quantity_status = "В наличии"
+                    if quantity < 2:
+                        quantity_status = "Скоро закончится"
 
-            if existing_item:
-                changed = False
-
-                if existing_item.name != name:
-                    existing_item.name = name
-                    changed = True
-
-                if existing_item.price != price:
-                    existing_item.price = price
-                    changed = True
-
-                if existing_item.quantity != quantity:
-                    existing_item.quantity = quantity
-                    changed = True
-
-                if existing_item.available != available:
-                    existing_item.available = available
-                    changed = True
-
-                if existing_item.quantity_status != quantity_status:
-                    existing_item.quantity_status = quantity_status
-                    changed = True
-
-                if changed:
-                    items_to_update.append(existing_item)
-            else:
-                item = Item(
+                Item.objects.create(
                     name=name,
-                    article=str(article_raw).strip(),
+                    article=article,
                     price=price,
                     available=available,
                     quantity_status=quantity_status,
                     category_id=268,
                     quantity=quantity,
                 )
-                items_to_create.append(item)
 
-        if items_to_create:
-            Item.objects.bulk_create(items_to_create, batch_size=500)
-            print(f"Создано новых товаров: {len(items_to_create)}")
-
-        if items_to_update:
-            Item.objects.bulk_update(
-                items_to_update,
-                ["name", "price", "quantity", "available", "quantity_status"],
-                batch_size=500,
-            )
-            print(f"Обновлено товаров: {len(items_to_update)}")
-
-        self.check_actuality(list_to_delete, list_actual)
-
+        self.check_actuality(list_to_delite, list_actual)
         self.stdout.write(self.style.SUCCESS("Successfully updated products"))
